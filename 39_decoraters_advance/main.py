431195e3fa3b6f4be7c8802c484419117dfdaf9c
codexAbhi007 <codewithabhi2024@gmail.com>
2024-06-25 23:14:39 +0530

    first commit

--- 39_decoraters_advance/main.py
+++ 39_decoraters_advance/main.py
@@ -20,4 +20,32 @@ def decorater_func(msg):
       
     return wrapper_func
 
-#instead of printing the message we can 
+#instead of printing the message we can pass a fx as an aurgument to the decorated func
+def decorater_func(original_function):
+    def wrapper_func():
+        print(f"Wrapper executed this before {original_function.__name__}")
+
+        original_function()
+
+        print(f"Wrapper executed this after {original_function.__name__}")
+
+      
+    return wrapper_func
+
+#@ example 1 without decorator
+def display():
+    print("Display Func!")
+
+decorated_display = decorater_func(display)
+decorated_display()
+
+## Real decorater!
+@decorater_func
+def display():
+    print("Display Func!")
+
+# decorated_display = decorater_func(display)
+# decorated_display()
+
+display()
+
