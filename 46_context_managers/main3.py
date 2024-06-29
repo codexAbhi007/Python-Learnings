1d5934504ff894ed9fe56fda624949e7035145b0
codexAbhi007 <codewithabhi2024@gmail.com>
2024-06-29 17:43:07 +0530

    first commit

--- 46_context_managers/main3.py
+++ 46_context_managers/main3.py
@@ -1,6 +1,38 @@
+#! TO run this file correctly do `code -r 46_context_managers` in terminal
+
 import os 
+from contextlib import contextmanager
 
-print(os.getcwd())
 
-os.chdir('sample_dir_1')
+##without context manager we have to manually switch directories and do our work and again change back
+# cwd = str(os.getcwd())
+# os.chdir('sample_dir_1')
+# print(os.listdir())
+# os.chdir(cwd)
+
+
+
+# os.chdir('sample_dir_2')
+# print(os.listdir())
+# os.chdir(cwd)
+# print(os.getcwd())
+
+
+##With context manager
+
+@contextmanager
+def change_dir(destination):
+    try:
+        cwd = os.getcwd()
+        os.chdir(destination)
+        yield #the thing we yeild goes to with as variable 
+    finally:
+        os.chdir(cwd)
+
+with change_dir('sample_dir_1'):
+    print(os.listdir())
+
+with change_dir('sample_dir_2'):
+    print(os.listdir())
+
 print(os.getcwd())
